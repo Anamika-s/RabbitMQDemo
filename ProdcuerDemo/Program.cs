b99fcf916c3cf136e3ba13c7c824023b4ec148ca
Anamika <anamika.sawhney22@gmail.com>
2021-03-17 19:56:33 +0530

    added prodcuer

---
+++ ProdcuerDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ProdcuerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 var factory = new ConnectionFactory
+                {
+                    Uri
+                    = new Uri("amqp://guest:guest@http://13.235.79.105:5672")
+                };
+                var connection = factory.CreateConnection();
+                var channel = connection.CreateModel();
+                channel.QueueDeclare("demoQueue", durable: true, exclusive: false,
+                    autoDelete: false, arguments: null);
+                var message = new { Name = "Producer", Message = "Hello" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                channel.BasicPublish("", "demoQueue", null, body);
+            }
+        }
+    }
+
+     
