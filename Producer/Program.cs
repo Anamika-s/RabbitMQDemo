fe210c18488aa0ebeb094d36c537a94f88cf5ce6
Anamika <anamika.sawhney22@gmail.com>
2021-03-17 19:42:28 +0530

    Add project files.

---
+++ Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri
+                = new Uri("amqp://guest:guest@http://13.235.79.105:5672")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("demoQueue", durable: true, exclusive: false,
+                autoDelete: false, arguments: null);
+            var message = new { Name = "Producer", Message = "Hello" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            channel.BasicPublish("",   "demoQueue", null, body);
+        }
+    }
+}
